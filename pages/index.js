9ca4a8785b64ff078f92392d8b6faf226af089fa
Jonathan Jeding <jonathan.jeding@gmail.com>
2021-03-03 14:19:45 +0100

    Reademe file changes

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,8 @@ export default function Home() {
       <main>
         <div className=""></div>
         <Articles></Articles>
+        <Articles></Articles>
+        <Articles></Articles>
       </main>
     </div>
   );
